463f8bb026b54b0876dd793d8cd0c8fe87ebb6fd
Vishnu <vishnudeepanp@gmail.com>
2024-03-31 19:57:43 +0530

    Version-1
    release-apk-version-1

---
+++ lib/features/prompt/bloc/prompt_event.dart
@@ -0,0 +1,16 @@
+part of 'prompt_bloc.dart';
+
+@immutable
+sealed class PromptEvent {}
+
+
+class PromptInitialEvent extends PromptEvent{
+
+}
+
+
+class PromptEnteredEvent extends PromptEvent{
+  final String prompt;
+
+  PromptEnteredEvent({required this.prompt});
+}
--- lib/features/prompt/ui/create_prompt_screen.dart
+++ lib/features/prompt/ui/create_prompt_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../bloc/prompt_bloc.dart';
 
 class CreatePromptScreen extends StatefulWidget {
   const CreatePromptScreen({super.key});
@@ -9,74 +13,174 @@ class CreatePromptScreen extends StatefulWidget {
 
 class _CreatePromptScreenState extends State<CreatePromptScreen> {
   TextEditingController controller = TextEditingController();
+  final PromptBloc promptBloc = PromptBloc();
+
+  bool _keyboardIsVisible = false;
+
+  @override
+  void initState() {
+    promptBloc.add(PromptInitialEvent());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0), // Set the preferred height here
-        child: AppBar(
-          title: const Text("AI Image Generator"),
-          centerTitle: true,
-          titleSpacing: 0.0,
-          toolbarHeight: 60.0, // Set the toolbar height to match the preferredSize
+    return GestureDetector(
+      onTap: () {
+        if (_keyboardIsVisible) {
+          FocusScope.of(context).unfocus();
+        }
+      },
+      onPanUpdate: (details) {
+        if (!_keyboardIsVisible) return;
+        setState(() {
+          _keyboardIsVisible = false;
+        });
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60.0),
+          child: AppBar(
+            title: const Text("AI Image Generator"),
+            centerTitle: true,
+            titleSpacing: 0.0,
+            toolbarHeight: 60.0,
+          ),
         ),
-      ),
-
-
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-              child: Container(
-            color: Colors.deepPurple,
-          )),
-          Container(
-            color: Colors.grey.shade900,
-            width: MediaQuery.of(context).size.width,
-            height: 240,
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Enter your Prompt : ",),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
+        body: BlocConsumer<PromptBloc, PromptState>(
+          bloc: promptBloc,
+          listener: (context, state) {},
+          builder: (context, state) {
+            switch (state.runtimeType) {
+              case PromptGeneratingImageLoadState:
+                return Container(
+                  color: Colors.black,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Shimmer.fromColors(
+                          baseColor: Colors.white10!,
+                          highlightColor: Colors.lightGreenAccent!,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width / 2,
+                            height: 10,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          'Generating Image...',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Please wait... It may take some time',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[300],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.maxFinite,
-                  height: 55,
-                  child: ElevatedButton.icon(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              20), // Set border radius to 0 for a rectangular shape
+                );
+
+              case PromptGeneratingImageErrorState:
+                return const Center(
+                  child: Text("Something Went Wrong"),
+                );
+
+              case PromptGeneratingImageSuccessState:
+                final successState =
+                state as PromptGeneratingImageSuccessState;
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.width,
+                        width: double.maxFinite,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fitWidth,
+                            image: MemoryImage(successState.uint8list),
+                          ),
                         ),
                       ),
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.deepPurple),
-                    ),
-                    onPressed: () {},
-                    icon: const Icon(Icons.generating_tokens_outlined),
-                    label: const Text("Generate Image"),
+                      SizedBox(
+                        height: MediaQuery.of(context).viewInsets.bottom,
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height-MediaQuery.of(context).size.width,
+                        color: Colors.grey.shade900,
+                        padding: const EdgeInsets.all(24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Enter your Prompt : "),
+                            const SizedBox(height: 20),
+                            TextField(
+                              controller: controller,
+                              maxLines: calculateMaxLines(context),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: double.maxFinite,
+                              height: 55,
+                              child: ElevatedButton.icon(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                  MaterialStateProperty.all(
+                                      Colors.deepPurple),
+                                ),
+                                onPressed: () {
+                                  if (controller.text.isNotEmpty) {
+                                    promptBloc.add(PromptEnteredEvent(
+                                        prompt: controller.text));
+                                  }
+                                },
+                                icon: const Icon(
+                                    Icons.generating_tokens_outlined),
+                                label: const Text("Generate Image"),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-        ],
+                );
+
+              default:
+                return const SizedBox();
+            }
+          },
+        ),
       ),
     );
+  }
+
+  int calculateMaxLines(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double estimatedLineHeight = 130.0;
+    int maxLines = (screenHeight / estimatedLineHeight).floor();
+    return maxLines;
   }
 }
